ab71d96821ccc9c286f224b4870221d32e175f6a
Chenzy <2684474862@qq.com>
2026-07-21 06:23:15 +0800

    refactor: distinguish local margin rejection from exchange errors

--- connector/src/binancefutures/mod.rs
+++ connector/src/binancefutures/mod.rs
@@ -291,17 +291,10 @@ impl Connector for BinanceFutures {
                                 available_balance = decision.available_balance,
                                 "Skipping order locally because available margin is insufficient."
                             );
-                            let error = BinanceFuturesError::OrderError {
-                                code: -2019,
-                                msg: format!(
-                                    "Local margin guard rejected order: required {:.8}, available {:.8}",
-                                    decision.required_balance, decision.available_balance
-                                ),
-                            };
                             if let Some(order) = order_manager
                                 .lock()
                                 .unwrap()
-                                .update_submit_fail(&client_order_id, &error)
+                                .update_from_rest_fail(&client_order_id, Some(Status::Expired))
                             {
                                 tx.send(PublishEvent::LiveEvent(LiveEvent::Order {
                                     symbol,
@@ -316,7 +309,7 @@ impl Connector for BinanceFutures {
                             if let Some(order) = order_manager
                                 .lock()
                                 .unwrap()
-                                .update_submit_fail(&client_order_id, &error)
+                                .update_from_rest_fail(&client_order_id, Some(Status::Expired))
                             {
                                 tx.send(PublishEvent::LiveEvent(LiveEvent::Order {
                                     symbol,
