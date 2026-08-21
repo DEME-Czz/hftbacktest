7b3bdffb17754a940ce7cc3e81e1077760c5100a
任务管理 <42460534+DEME-Czz@users.noreply.github.com>
2026-08-21 17:19:10 +0800

    refactor: make market batch events symbol aware

--- app/src/connector.rs
+++ app/src/connector.rs
@@ -3,14 +3,14 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-use hftbacktest::types::{LiveEvent, Order};
+use hftbacktest::types::Order;
 use tokio::sync::mpsc::UnboundedSender;
 
 /// A message will be received by the publisher thread and then published to the bots.
 pub enum PublishEvent {
-    BatchStart(u64),
-    BatchEnd(u64),
-    LiveEvent(LiveEvent),
+    BatchStart(String),
+    BatchEnd(String),
+    LiveEvent(hftbacktest::types::LiveEvent),
     RegisterInstrument {
         id: u64,
         symbol: String,
@@ -30,30 +30,13 @@ pub trait ConnectorBuilder {
 
 /// Provides an interface for connecting with an exchange or broker for a live bot.
 pub trait Connector {
-    /// Registers an instrument to be traded through this connector.
     fn register(&mut self, symbol: String);
-
-    /// Returns an [`OrderManager`].
     fn order_manager(&self) -> Arc<Mutex<dyn GetOrders + Send + 'static>>;
-
-    /// Runs the connector, establishing the connection and preparing to exchange information such
-    /// as data feed and orders. This method should not block, and any response should be returned
-    /// through the channel using [`PublishEvent`]. The returned error should not be related to the
-    /// exchange; instead, it should indicate a connector internal error.
     fn run(&mut self, tx: UnboundedSender<PublishEvent>);
-
-    /// Submits a new order. This method should not block, and the response should be returned
-    /// through the channel using [`PublishEvent`]. The returned error should not be related to the
-    /// exchange; instead, it should indicate a connector internal error.
     fn submit(&self, symbol: String, order: Order, tx: UnboundedSender<PublishEvent>);
-
-    /// Cancels an open order. This method should not block, and the response should be returned
-    /// through the channel using [`PublishEvent`]. The returned error should not be related to the
-    /// exchange; instead, it should indicate a connector internal error.
     fn cancel(&self, symbol: String, order: Order, tx: UnboundedSender<PublishEvent>);
 }
 
-/// Provides `orders` method to get the current working orders.
 pub trait GetOrders {
     fn orders(&self, symbol: Option<String>) -> Vec<Order>;
 }
